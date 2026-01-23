bf9edcfbdf3d2041282a511bd28c6da402d251be
Souvik313 <roysouvik8220@gmail.com>
2026-01-24 02:02:35 +0530

    Update CORS to allow Vercel frontend

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: "https://vibely-mauve.vercel.app",
   credentials: true
 }));
 app.use(express.json());
